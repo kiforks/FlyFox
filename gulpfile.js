6c12332f6791a109e495167e34caf095956bd967
kiforks <ks.kifor@gmail.com>
2020-09-09 17:24:52 +0300

    Added bootstrap grid

--- gulpfile.js
+++ gulpfile.js
@@ -279,7 +279,7 @@ function ttfConversion() {
 //Favicons
 function favIcons(done) {
   realFavicon.generateFavicon({
-    masterPicture: `${path.src.favIcons}.svg`,  // Change format to png if you need it. WARNING! NAME MUST BE 'favicon'
+    masterPicture: `${path.src.favIcons}.png`,  // Change format to png/svg if you need it. WARNING! NAME MUST BE 'favicon'
     dest: path.build.favIcons,
     iconsPath: '/',
     design: {
---
+++ src/js/scripts/init/fullpage__init.js
@@ -0,0 +1,6 @@
+new fullpage('#fullpage', {
+  autoScrolling: true,
+  scrollHorizontally: true,
+  normalScrollElements: '#pagepiling',
+  licenseKey: '98CABA08-F152482D-888A56C4-514642C1'
+});
